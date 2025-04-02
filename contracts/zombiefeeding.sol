0e885a748a5867a6c6253b3c2ba6ff746f4b6f37
Girgis Sami <jirgissamy78@gmail.com>
2025-04-02 14:38:22 +0200

    Create feedAndMultiply function

--- contracts/zombiefeeding.sol
+++ contracts/zombiefeeding.sol
@@ -3,4 +3,6 @@ pragma solidity ^0.8.0;
 
 import "./zombiefactory.sol";
 
-contract ZombieFeeding is ZombieFactory {}
+contract ZombieFeeding is ZombieFactory {
+    function feedAndMultiply(uint _zombiId, uint _targetDna) public {}
+}
