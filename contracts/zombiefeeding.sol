cc6628b02658d195a4e2a17a8dd96c674e19c823
Girgis Sami <jirgissamy78@gmail.com>
2025-04-02 16:04:30 +0200

    Create KittyInterface

--- contracts/zombiefeeding.sol
+++ contracts/zombiefeeding.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.0;
 
 import "./zombiefactory.sol";
 
+contract KittyInterface {}
+
 contract ZombieFeeding is ZombieFactory {
     function feedAndMultiply(uint _zombiId, uint _targetDna) public {
         require(msg.sender == zombieToOwner[_zombiId]);
