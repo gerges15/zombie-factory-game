60dea88b573221f70863f0b49188d1da76fb1242
Girgis Sami <jirgissamy78@gmail.com>
2025-04-02 15:08:49 +0200

    Add condition

--- contracts/zombiefeeding.sol
+++ contracts/zombiefeeding.sol
@@ -4,5 +4,7 @@ pragma solidity ^0.8.0;
 import "./zombiefactory.sol";
 
 contract ZombieFeeding is ZombieFactory {
-    function feedAndMultiply(uint _zombiId, uint _targetDna) public {}
+    function feedAndMultiply(uint _zombiId, uint _targetDna) public {
+        require(msg.sender == zombieToOwner[_zombiId]);
+    }
 }
