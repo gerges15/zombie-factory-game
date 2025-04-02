5e80888bd46e5b942b3b1fc03d2c9f80d2575a34
Girgis Sami <jirgissamy78@gmail.com>
2025-04-02 15:24:17 +0200

    Sure _targetDna is not more than 16 digit

--- contracts/zombiefeeding.sol
+++ contracts/zombiefeeding.sol
@@ -7,5 +7,6 @@ contract ZombieFeeding is ZombieFactory {
     function feedAndMultiply(uint _zombiId, uint _targetDna) public {
         require(msg.sender == zombieToOwner[_zombiId]);
         Zombie storage myZombie = zombies[_zombiId];
+        _targetDna %= myZombie.dna;
     }
 }
