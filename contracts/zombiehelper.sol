eb5ae5090084ebb8a4ec28612c244f1ffb42fd1d
Girgis Sami <jirgissamy78@gmail.com>
2025-04-10 11:51:12 +0200

    Add aboveLevel modifier

--- contracts/zombiehelper.sol
+++ contracts/zombiehelper.sol
@@ -3,4 +3,9 @@ pragma solidity ^0.8.0;
 
 import "./zombiefeeding.sol";
 
-contract ZombieHelper is ZombieFeeding {}
+contract ZombieHelper is ZombieFeeding {
+    modifier aboveLevel(uint _level, uint _zombieId) {
+        require(zombies[_zombieId].level >= _level);
+        _;
+    }
+}
