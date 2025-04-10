92227fb5054bfb47fac7b5ec70ce4631c1ce5b94
Girgis Sami <jirgissamy78@gmail.com>
2025-04-10 12:24:27 +0200

    Modify getZombiesByOwner function

--- contracts/zombiehelper.sol
+++ contracts/zombiehelper.sol
@@ -27,5 +27,9 @@ contract ZombieHelper is ZombieFeeding {
 
     function getZombiesByOwner(
         address _owner
-    ) external view returns (uint[] memory) {}
+    ) external view returns (uint[] memory) {
+        uint[] memory result = new uint[](ownerZombieCount[_owner]);
+
+        return result;
+    }
 }
