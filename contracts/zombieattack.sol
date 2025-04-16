d6835b0772a4cd465e4c2f906b23a64b62addfda
Girgis Sami <jirgissamy78@gmail.com>
2025-04-16 18:23:33 +0200

    Add randMod function for generating pseudo-random numbers

--- contracts/zombieattack.sol
+++ contracts/zombieattack.sol
@@ -3,4 +3,16 @@ pragma solidity ^0.8.0;
 
 import "./zombiehelper.sol";
 
-contract ZombieAttack is ZombieHelper {}
+contract ZombieAttack is ZombieHelper {
+    uint randNonce = 0;
+
+    function randMod(uint _modulus) internal returns (uint) {
+        randNonce++;
+        return
+            uint(
+                keccak256(
+                    abi.encodePacked(block.timestamp, msg.sender, randNonce)
+                )
+            ) % 100;
+    }
+}
