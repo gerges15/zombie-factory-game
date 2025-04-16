a78b341e5d15117e2c47ae8f6d78eda3d6bed772
Girgis Sami <jirgissamy78@gmail.com>
2025-04-16 18:56:03 +0200

    Change the function definition  such that it uses the modifier ownerOf

--- contracts/zombiehelper.sol
+++ contracts/zombiehelper.sol
@@ -28,7 +28,7 @@ contract ZombieHelper is ZombieFeeding {
     function changeName(
         uint _zombieId,
         string calldata _newName
-    ) external aboveLevel(2, _zombieId) {
+    ) external aboveLevel(2, _zombieId) ownerOf(_zombieId) {
         require(zombieToOwner[_zombieId] == msg.sender);
         zombies[_zombieId].name = _newName;
     }
@@ -36,7 +36,7 @@ contract ZombieHelper is ZombieFeeding {
     function changeDna(
         uint _zombieId,
         uint _newDna
-    ) external aboveLevel(20, _zombieId) {
+    ) external aboveLevel(20, _zombieId) ownerOf(_zombieId) {
         require(zombieToOwner[_zombieId] == msg.sender);
         zombies[_zombieId].dna = _newDna;
     }
