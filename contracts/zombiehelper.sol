29b7311224650d15b35eed88301dc0c000cf3cc3
Girgis Sami <jirgissamy78@gmail.com>
2025-04-23 14:36:40 +0200

    refactor: remove dead code

--- contracts/zombiehelper.sol
+++ contracts/zombiehelper.sol
@@ -29,7 +29,6 @@ contract ZombieHelper is ZombieFeeding {
         uint _zombieId,
         string calldata _newName
     ) external aboveLevel(2, _zombieId) ownerOf(_zombieId) {
-        require(zombieToOwner[_zombieId] == msg.sender);
         zombies[_zombieId].name = _newName;
     }
 
@@ -37,7 +36,6 @@ contract ZombieHelper is ZombieFeeding {
         uint _zombieId,
         uint _newDna
     ) external aboveLevel(20, _zombieId) ownerOf(_zombieId) {
-        require(zombieToOwner[_zombieId] == msg.sender);
         zombies[_zombieId].dna = _newDna;
     }
 
