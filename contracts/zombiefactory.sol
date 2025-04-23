8ec09e9c9dc3889306d02e6b18a765a96f2fa78a
Girgis Sami <jirgissamy78@gmail.com>
2025-04-23 14:42:08 +0200

    modify: add to more properties winCount and lossCount to Zombie struct

--- contracts/zombiefactory.sol
+++ contracts/zombiefactory.sol
@@ -17,6 +17,8 @@ contract ZombieFactory is Ownable {
         uint dna;
         uint32 level;
         uint32 readyTime;
+        uint16 winCount;
+        uint16 lossCount;
     }
 
     function createRandomZombie(string memory _name) public {
@@ -27,7 +29,7 @@ contract ZombieFactory is Ownable {
 
     function _createZombie(string memory _name, uint _dna) internal {
         zombies.push(
-            Zombie(_name, _dna, 1, uint32(block.timestamp + cooldownTime))
+            Zombie(_name, _dna, 1, uint32(block.timestamp + cooldownTime), 0, 0)
         );
         uint id = zombies.length - 1;
         zombieToOwner[id] = msg.sender;
